3ee31102e97a5e2b129c0d2a1ba73eb326d16f18
ryusei42 <ryuseitakata27@gmail.com>
2020-11-15 14:08:28 +0900

    1st commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import json
+from requests_oauthlib import OAuth1Session
+
+API_KEY = 'rf9fsvEsdjodaLoyMv9NIbdu3'
+API_SECRET_KEY = 'RsSLURxwDkmeWWpJYrqRikeeVqxHZjXJfA9y9dyteB5ECEG610'
+ACCESS_TOKEN = '2916609852-spdf353YECyzBrXkXaG0UdnOzMBpn50cHatVNTs'
+ACCESS_SECRET_TOKEN = 'sI5wOmejsTYzj0WGEPtah3RgY4Xk6fQCEuH5F217SoQq3'
+
+# 情報の取得
+twitter = OAuth1Session(API_KEY, API_SECRET_KEY, ACCESS_TOKEN, ACCESS_SECRET_TOKEN)
+url = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
+params = {'count':5} #辞書型 個数指定
+
+res = twitter.get(url, params = params)
+# print(res.status_code) 200
+
+timelines = json.loads(res.text)
+# print(timelines[0])
+timeline = timelines[0]
+# print(timeline["user"]["name"]) #ユーザーの名前を取得
+# print(timeline['user']['description']) #ユーザーのユーザーのプロフィール文を取得
+# print(timeline['text']) #ユーザーのツイートの文を取得
+
+#投稿
+twitter = OAuth1Session(API_KEY, API_SECRET_KEY, ACCESS_TOKEN, ACCESS_SECRET_TOKEN)
+url = 'https://api.twitter.com/1.1/statuses/update.json'
+params = {'status' : 'APIお試し'}
+res = twitter.post(url, params = params)
+print(res)
+
+
+
+
+
+
